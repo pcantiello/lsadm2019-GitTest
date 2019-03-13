8e26eb1d6efad54f9290ebe79eff08e79eea84c0
Pasquale Cantiello <pasquale.cantiello@unicampania.it>
2019-03-13 16:58:30 +0100

    Grandi modifiche al codice della main activity

--- app/src/main/java/it/unicampania/lsadm/gittest/MainActivity.kt
+++ app/src/main/java/it/unicampania/lsadm/gittest/MainActivity.kt
@@ -3,10 +3,14 @@ package it.unicampania.lsadm.gittest
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
+// Classe per la main activity
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val a = 10
+        
     }
 }
